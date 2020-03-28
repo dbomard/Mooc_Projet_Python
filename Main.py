0acf8fa1f3316b18cacc064f90bb412e040e59e6
David Bomard <david.bomard@gmail.com>
2020-03-28 09:10:48 +0100

    Initial commit

---
+++ Main.py
@@ -0,0 +1,16 @@
+import math
+
+
+def hexagone(point, longueur, col, centre, rayon):
+    pass  # TODO -- Créer une fonction qui dessine hexagone
+
+
+def deformation(point, centre, rayon):
+    pass  # TODO -- Calcule la déformation
+
+
+def pavage(inf_gauche, sup_droit, longueur, col, rayon):
+    pass  # TODO -- Dessine le pavage
+
+
+pass  # TODO -- code principal
