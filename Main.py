f45ba8f6a74c92b047d7156803d2269f837abacd
David Bomard <david.bomard@gmail.com>
2020-03-28 18:47:50 +0100

    Ajout des commentaires Ajout de la fonction pavé(a,b,c) Début de code de la fonction hexagone Debuguage et finalisation des fonctions de traçage

--- Main.py
+++ Main.py
@@ -1,29 +1,51 @@
-from math import pi, sin, cos, sqrt, acos, asin, atan2
+from math import pi, sin, cos, sqrt, acos, asin
 import turtle
+
+
+def calculate_sommets(center_point, rayon):
+    """
+    Calcule les coordonnées des points de l'hexagone et les renvoie sous formes d'objet
+    de 6 tuples
+    Entrées :
+        center_point: coordonnées du centre
+        rayon: rayon entre le centre et les points
+     Sortie :
+        sommets : objet contenant les 6 couples de points
+    """
+
+    # initialisation d'un objet de 6 tuples
+    sommets = [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)]
+
+    # calcul et remplissage des coordonnées des 6 sommets de l'hexagone
+    for i in range(len(sommets)):
+        sommets[i] = (center_point[0] + cos(2 * pi / 6 * i) * rayon, center_point[1] + sin(2 * pi / 6 * i) * rayon, 0)
+
+    return sommets
 
 
 def pave(point1, point2, point3, point4, couleur):
     """
     peint un pavé ayant pour sommets les 4 points p1 à p4 dont les coordonnées dans le plan sont
-    représentés sous forme de tuples(x, y)
+    représentés sous forme de tuples(x, y, z)
     Entrées :
         p1 : point n°1
         p2 : point n°2
         p3 : point n°3
         p4 : point n°4
         couleur : couleur du pavé
+
     """
+    turtle.up()
     turtle.goto(point1[0], point1[1])  # déplacer la tortue au point n°1
     turtle.down()  # commencer à dessiner
     turtle.color(couleur)  # changer la couleur de crayon
-    turtle.beginfill()  # commencer à remplir
+    turtle.begin_fill()  # commencer à remplir
     turtle.goto(point2[0], point2[1])  # relier tous les points
     turtle.goto(point3[0], point3[1])
     turtle.goto(point4[0], point4[1])
     turtle.goto(point1[0], point1[1])
     turtle.end_fill()  # clôturer et remplir le pavé
     turtle.up()  # lever le crayon
-import math
 
 
 def hexagone(point, longueur, col, centre, rayon):
@@ -38,29 +60,18 @@ def hexagone(point, longueur, col, centre, rayon):
         rayon : rayon de la sphère de déformation
     """
 
-    x, y, z = point
+    # calcul des coordonnées des sommets de l'hexagone par la fonction 'calculate_sommets'
+    points_sommets = calculate_sommets(point, longueur)
 
-    # dessin du pavé n°1
-    p1 = (x, y, z)
-    p2 = (x + cos(2 * pi / 3) * longueur, y + sin(pi / 3) * longueur, 0)
-    p3 = (x + cos(pi / 3) * longueur, y + sin(pi / 3) * longueur, 0)
-    p4 = (x + longueur, y, 0)
-    pave(p1, p2, p3, p4, col)
+    # transformation de la déformation des coordonnées des sommets selon la sphère
+    point_centre = deformation(point, centre, rayon)  # d'abord le point central de l'hexagone
+    for p in range(6):
+        points_sommets[p] = deformation(points_sommets[p], centre, rayon)  # puis les 6 sommets
 
-    # dessin du pavé n°2
-    p1 = (x, y, z)
-    p2 = (x + longueur, y, 0)
-    p3 = (x + cos(pi / 3) * longueur, y - sin(pi / 3) * longueur, 0)
-    p4 =  (x + cos(2 * pi / 3) * longueur, y - sin(pi / 3) * longueur, 0)
-    pave(p1, p2, p3, p4, col)
-    pass  # TODO -- Créer une fonction qui dessine hexagone
-
-    # dessin du pavé n°3
-    p1 = (x, y, z)
-    p2 = (x + longueur, y, 0)
-    p3 = (x + cos(pi / 3) * longueur, y - sin(pi / 3) * longueur, 0)
-    p4 = (x + cos(2 * pi / 3) * longueur, y - sin(pi / 3) * longueur, 0)
-    pave(p1, p2, p3, p4, col)
+    # dessins des pavés par la fonction 'pave'
+    pave(point_centre, points_sommets[2], points_sommets[1], points_sommets[0], col[0])  # dessin du pavé n°1
+    pave(point_centre, points_sommets[0], points_sommets[5], points_sommets[4], col[1])  # dessin du pavé n°2
+    pave(point_centre, points_sommets[4], points_sommets[3], points_sommets[2], col[2])  # dessin du pavé n°3
 
 
 def deformation(p, centre, rayon):
@@ -96,13 +107,11 @@ def deformation(p, centre, rayon):
             if centre[2] > 0:
                 zprim = -zprim
     return xprim, yprim, zprim
-def deformation(point, centre, rayon):
-    pass  # TODO -- Calcule la déformation
 
 
 def pavage(inf_gauche, sup_droit, longueur, col, rayon):
-    pass  # TODO -- Dessine le pavage
+    turtle.reset()
+    hexagone((100, 100, 0), 10, ('red', 'black', 'blue'), (120, 120, 0), 200)
 
 
-pass  # TODO -- code principal
-
+pavage(0, 0, 0, 0, 0)
